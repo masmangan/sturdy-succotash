b00f486bf72cddd8ba98f157b7aea952ed17961e
Marco Mangan <marco.mangan@pucrs.br>
2019-09-16 21:53:08 -0300

    #2

---
+++ src/main/java/pucrs/qp/adt/Node.java
@@ -0,0 +1,25 @@
+package pucrs.qp.adt;
+// 04
+public class Node {
+	private String data;
+	private Node next;
+	public String getData() {
+		return data;
+	}
+	public void setData(String data) {
+		this.data = this.data;
+	}
+	public Node getNext() {
+		return next;
+	}
+	public void setNext(Node next) {
+		this.next = next;
+	}
+	public Node(String data, Node next) {
+		this.data = this.data;
+		next = next;
+	}
+	public String toString() {
+		return data;
+	}
+}
